c7acdbca66c8d2a5593b8fa374ef157d25863e51
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-07-29 16:03:39 +0800

    postioning done

--- resources/js/components/sap1/Intruction.vue
+++ resources/js/components/sap1/Intruction.vue
@@ -2,5 +2,36 @@
 
 </script>
 <template>
-    
+    <!-- //how to center -->
+<main class="max-w-7xl mx-auto mt-2 p-8  ">
+    <section class="">
+        <div class="grid grid-cols-12  w-full h-full content-center gap-2">
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+            <div class=" h-24 bg-blue-600  text-white rounded p-2 text-xs flex flex-col items-center justify-center">1</div>
+        </div>
+
+    </section>
+</main>
 </template>
--- resources/js/components/sap1/SimulationSpace.vue
+++ resources/js/components/sap1/SimulationSpace.vue
@@ -3,13 +3,97 @@ import Box from './Box.vue'
 
 const components = [
 {
+    id:'pc',
   title: 'Program Counter (PC)',
-  value: '0001',
+  value: '0000',
   row: 2,        // Start at row 2
-  col: 4,        // Start at col 4
+  col: 2,        // Start at col 4
   rowSpan: 2,    // Covers rows 2 and 3
   colSpan: 2     // Covers cols 4 and 5
-}
+},
+{
+    id:'mar',
+  title: 'Memory Address Register (MAR)',
+  value: '0000',
+  row: 5,        // Start at row 2
+  col: 2,        // Start at col 4
+  rowSpan: 2,    // Covers rows 2 and 3
+  colSpan: 2     // Covers cols 4 and 5
+},
+{
+    id:'prom',
+  title: '16x8 PROM',
+  value: '0000',
+  row: 8,        // Start at row 2
+  col: 2,        // Start at col 4
+  rowSpan: 2,    // Covers rows 2 and 3
+  colSpan: 2     // Covers cols 4 and 5
+},
+{
+    id:'ir',
+  title: 'Instruction Register (IR)',
+  value: '0000',
+  row: 11,        // Start at row 2
+  col: 2,        // Start at col 4
+  rowSpan: 2,    // Covers rows 2 and 3
+  colSpan: 2     // Covers cols 4 and 5
+},
+
+{
+    id:'con',
+  title: 'Control Unit CON',
+  value: '0000',
+  row: 14,        // Start at row 2
+  col: 2,        // Start at col 4
+  rowSpan: 2,    // Covers rows 2 and 3
+  colSpan: 2     // Covers cols 4 and 5
+},
+
+{
+    id:'ar',
+  title: 'A Register',
+  value: '0000',
+  row: 2,        // Start at row 2
+  col: 12,        // Start at col 4
+  rowSpan: 2,    // Covers rows 2 and 3
+  colSpan: 2     // Covers cols 4 and 5
+},
+{
+    id:'alu',
+  title: 'Arithmetic Logic Unit (ALU)',
+  value: '0000',
+  row: 5,        // Start at row 2
+  col: 12,        // Start at col 4
+  rowSpan: 2,    // Covers rows 2 and 3
+  colSpan: 2     // Covers cols 4 and 5
+},
+{
+    id:'br',
+  title: 'B Register',
+  value: '0000',
+  row: 8,        // Start at row 2
+  col: 12,        // Start at col 4
+  rowSpan: 2,    // Covers rows 2 and 3
+  colSpan: 2     // Covers cols 4 and 5
+},
+{
+    id:'or',
+  title: 'Output Register',
+  value: '0000',
+  row: 11,        // Start at row 2
+  col: 12,        // Start at col 4
+  rowSpan: 2,    // Covers rows 2 and 3
+  colSpan: 2     // Covers cols 4 and 5
+},
+{
+    id:'bd',
+  title: 'Binary Display',
+  value: '0000',
+  row: 14,        // Start at row 2
+  col: 12,        // Start at col 4
+  rowSpan: 2,    // Covers rows 2 and 3
+  colSpan: 2     // Covers cols 4 and 5
+},
 
 ]
 </script>
@@ -18,7 +102,7 @@ const components = [
 <div class="grid grid-cols-16 gap-px w-full h-full" style="grid-template-columns: repeat(16, 45px); grid-template-rows: repeat(16, 48px);">
   <Box
     v-for="c in components"
-    :key="c.title"
+    :key="c.id"
     :title="c.title"
     :value="c.value"
     :style="{
